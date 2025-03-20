4b37048c39f4de7e5e74fd8dc291841b9db94424
owl352 <kokosinca123@gmail.com>
2025-03-21 00:08:06 +0300

    initial commit

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2312
 
+RED='\033[0;31m'
+NC='\033[0m'
+
 TARGET=wasm32-unknown-unknown
 
 OUTPUT_DIR="${PWD}/wasm"
@@ -9,8 +12,18 @@ PROFILE=release
 
 OUTPUT_FILE="${PWD}/wasm/pshenmic_dpp_bg.wasm"
 
-BUILD_COMMAND="cargo build --config net.git-fetch-with-cli=true --target=${TARGET} ${PROFILE_ARG}"
-BINDGEN_COMMAND="wasm-bindgen --out-dir=${OUTPUT_DIR} --target=web --omit-default-module-path ${PWD}/target/${TARGET}/${PROFILE}/pshenmic_dpp.wasm"
+FEATURES_BASE="--features="
+FEATURES_DEFAULT="all"
+FEATURES_ARG="${FEATURES_BASE}${FEATURES_DEFAULT}"
+
+if [ -n "$FEATURES" ]; then
+  echo -e "using feature: ${RED}'${FEATURES}'${NC}"
+  FEATURES_ARG="${FEATURES_BASE}${FEATURES}"
+else
+  echo -e "using default feature: ${RED}'all'${NC}"
+fi
+
+BINDGEN_COMMAND="wasm-pack build --out-dir=${OUTPUT_DIR}  ${PROFILE_ARG} --mode=normal --target=web -- --target=${TARGET} ${FEATURES_ARG}"
 
 if ! [[ -d ${OUTPUT_DIR} ]]; then
   mkdir -p "${OUTPUT_DIR}"
@@ -19,16 +32,7 @@ fi
 if [[ "${OSTYPE}" == "darwin"* ]]; then
   AR_PATH=$(command -v llvm-ar)
   CLANG_PATH=$(command -v clang)
-  AR=${AR_PATH} CC=${CLANG_PATH} ${BUILD_COMMAND}
   AR=${AR_PATH} CC=${CLANG_PATH} ${BINDGEN_COMMAND}
 else
-  ${BUILD_COMMAND}
   ${BINDGEN_COMMAND}
-fi
-
-if command -v wasm-opt &> /dev/null; then
-  echo "Optimizing wasm using Binaryen"
-  wasm-opt -tnh --flatten --rereloop -Oz --gufa -Oz --gufa -Oz  "$OUTPUT_FILE" -o "${OUTPUT_FILE}"
-else
-  echo "wasm-opt command not found. Skipping wasm optimization."
 fi
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,19 @@
 #![no_std]
 #![no_main]
 
+#[cfg(feature = "data_contract")]
 pub use pshenmic_dpp_data_contract;
+#[cfg(feature = "document_batch")]
 pub use pshenmic_dpp_document_batch;
+#[cfg(feature = "document_search")]
 pub use pshenmic_dpp_document_search;
+#[cfg(feature = "enums")]
 pub use pshenmic_dpp_enums;
+#[cfg(feature = "document")]
+pub use pshenmic_dpp_document;
+#[cfg(feature = "identity")]
 pub use pshenmic_dpp_identity;
+#[cfg(feature = "identity_transitions")]
 pub use pshenmic_dpp_identity_transitions;
+#[cfg(feature = "state_transition")]
 pub use pshenmic_dpp_state_transition;
