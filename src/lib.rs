d27636024529ca8213b378e2b9c0579d0fa9e05b
owl352 <kokosinca123@gmail.com>
2025-03-21 00:34:22 +0300

    lint

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,14 @@
 
 #[cfg(feature = "data_contract")]
 pub use pshenmic_dpp_data_contract;
+#[cfg(feature = "document")]
+pub use pshenmic_dpp_document;
 #[cfg(feature = "document_batch")]
 pub use pshenmic_dpp_document_batch;
 #[cfg(feature = "document_search")]
 pub use pshenmic_dpp_document_search;
 #[cfg(feature = "enums")]
 pub use pshenmic_dpp_enums;
-#[cfg(feature = "document")]
-pub use pshenmic_dpp_document;
 #[cfg(feature = "identity")]
 pub use pshenmic_dpp_identity;
 #[cfg(feature = "identity_transitions")]
