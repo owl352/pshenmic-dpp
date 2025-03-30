cd5d3205b67eaff4a72396a8ce4f15bb5706cd83
owl352 <kokosinca123@gmail.com>
2025-03-31 01:53:17 +0300

    tests

--- tests/Document.spec.js
+++ tests/Document.spec.js
@@ -1,7 +1,7 @@
 const assert = require('assert')
 const { describe, it, before } = require('mocha')
 const initWasm = require('./utils/wasm')
-const {document, dataContractId, ownerId, documentTypeName, revision, documentBytes, schema} = require('./mocks/Document')
+const {document, dataContractId, ownerId, documentTypeName, revision, documentBytes, schema, idBytes} = require('./mocks/Document')
 const {PlatformVersionWASM} = require("../wasm/pshenmic_dpp");
 
 let wasm
@@ -18,18 +18,22 @@ describe('Document', function () {
       assert.notEqual(documentInstance.__wbg_ptr, 0)
     })
 
-    it('should allows to convert Document to bytes', function () {
-      const documentInstance = new wasm.DocumentWASM(document, documentTypeName, revision, dataContractId, ownerId)
+    it('should allows to create Document from bytes and convert to bytes', function () {
       const dataContract = new wasm.DataContractWASM(schema, false)
-
-      const t = wasm.DocumentWASM.fromBytes(documentBytes, dataContract, 'note', PlatformVersionWASM.PLATFORM_V1)
-
-      assert.notEqual(documentInstance.__wbg_ptr, 0)
+      const documentInstance = wasm.DocumentWASM.fromBytes(documentBytes, dataContract, 'note')
 
       const bytes = documentInstance.toBytes(dataContract, 'note')
 
       assert.deepEqual(Array.from(bytes), documentBytes)
-
+      assert.notEqual(dataContract.__wbg_ptr, 0)
     })
+  })
+
+  describe('getters', function () {
+    it('should return document id', () => {
+      const documentInstance = new wasm.DocumentWASM(document, documentTypeName, revision, dataContractId, ownerId)
+
+      assert.deepEqual(Array.from(documentInstance.getId()), idBytes)
+    });
   })
 })
