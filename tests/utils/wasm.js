639aa9971d13b2fc9bc345a99f0cbba3faf1270e
owl352 <kokosinca123@gmail.com>
2025-04-01 12:42:59 +0300

    test babel

--- tests/utils/wasm.js
+++ tests/utils/wasm.js
@@ -1,5 +1,5 @@
 const wasm = require('../../dist/wasm/pshenmic_dpp')
-const wasmBytes = require('../../dist/wasm/pshenmic_dpp_bg')
+const {default: wasmBytes} = require('../../dist/wasm/pshenmic_dpp_bg')
 
 module.exports = () => {
   const binaryString = atob(wasmBytes)
