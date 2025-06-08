542a1e487ae232ed7ba8096c086521179bce54c4
owl352 <kokosinca123@gmail.com>
2025-06-09 00:21:57 +0300

    update

--- packages/batch/src/token_transitions/config_update.rs
+++ packages/batch/src/token_transitions/config_update.rs
@@ -1,6 +1,7 @@
 use crate::token_base_transition::TokenBaseTransitionWASM;
 use dpp::state_transition::batch_transition::TokenConfigUpdateTransition;
 use dpp::state_transition::batch_transition::token_config_update_transition::TokenConfigUpdateTransitionV0;
+use pshenmic_dpp_token_configuration_change_item::TokenConfigurationChangeItemWASM;
 use wasm_bindgen::JsValue;
 use wasm_bindgen::prelude::wasm_bindgen;
 
@@ -25,13 +26,14 @@ impl TokenConfigUpdateTransitionWASM {
     #[wasm_bindgen(constructor)]
     pub fn new(
         base: &TokenBaseTransitionWASM,
+        update_token_configuration_item: &TokenConfigurationChangeItemWASM,
         public_note: Option<String>,
     ) -> Result<TokenConfigUpdateTransitionWASM, JsValue> {
         Ok(TokenConfigUpdateTransitionWASM(
             TokenConfigUpdateTransition::V0(TokenConfigUpdateTransitionV0 {
-                base: Default::default(),
-                update_token_configuration_item: Default::default(),
-                public_note: None,
+                base: base.clone().into(),
+                update_token_configuration_item: update_token_configuration_item.clone().into(),
+                public_note,
             }),
         ))
     }
