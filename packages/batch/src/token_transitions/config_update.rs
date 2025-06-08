552e2525203ab72eff45ba8c85af63bbb7efc78e
owl352 <kokosinca123@gmail.com>
2025-06-09 00:31:56 +0300

    update

--- packages/batch/src/token_transitions/config_update.rs
+++ packages/batch/src/token_transitions/config_update.rs
@@ -1,6 +1,8 @@
+use dpp::state_transition::batch_transition::token_base_transition::token_base_transition_accessors::TokenBaseTransitionAccessors;
 use crate::token_base_transition::TokenBaseTransitionWASM;
 use dpp::state_transition::batch_transition::TokenConfigUpdateTransition;
 use dpp::state_transition::batch_transition::token_config_update_transition::TokenConfigUpdateTransitionV0;
+use dpp::state_transition::batch_transition::token_config_update_transition::v0::v0_methods::TokenConfigUpdateTransitionV0Methods;
 use pshenmic_dpp_token_configuration_change_item::TokenConfigurationChangeItemWASM;
 use wasm_bindgen::JsValue;
 use wasm_bindgen::prelude::wasm_bindgen;
@@ -36,5 +38,36 @@ impl TokenConfigUpdateTransitionWASM {
                 public_note,
             }),
         ))
+    }
+
+    #[wasm_bindgen(getter = base)]
+    pub fn get_base(&self) -> TokenBaseTransitionWASM {
+        self.0.base().clone().into()
+    }
+
+    #[wasm_bindgen(getter = public_note)]
+    pub fn get_public_note(&self) -> Option<String> {
+        self.clone().0.public_note_owned()
+    }
+
+    #[wasm_bindgen(getter = updateTokenConfigurationItem)]
+    pub fn get_update_token_configuration_item(&self) -> TokenConfigurationChangeItemWASM {
+        self.0.update_token_configuration_item().clone().into()
+    }
+
+    #[wasm_bindgen(setter = base)]
+    pub fn set_base(&mut self, base: TokenBaseTransitionWASM) {
+        self.0.set_base(base.into())
+    }
+
+    #[wasm_bindgen(setter = public_note)]
+    pub fn set_public_note(&mut self, note: Option<String>) {
+        self.0.set_public_note(note)
+    }
+
+    #[wasm_bindgen(setter = updateTokenConfigurationItem)]
+    pub fn set_update_token_configuration_item(&mut self, item: &TokenConfigurationChangeItemWASM) {
+        self.0
+            .set_update_token_configuration_item(item.clone().into())
     }
 }
