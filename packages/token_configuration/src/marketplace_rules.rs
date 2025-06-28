bfbdf2cccce0ed1ee339841861cd5c111caad044
owl352 <kokosinca123@gmail.com>
2025-06-28 05:18:00 +0300

    lint

--- packages/token_configuration/src/marketplace_rules.rs
+++ packages/token_configuration/src/marketplace_rules.rs
@@ -54,7 +54,11 @@ impl TokenMarketplaceRulesWASM {
     }
 
     #[wasm_bindgen(setter = "tradeModeChangeRules")]
-    pub fn set_trade_mode_change_rules(&mut self, trade_mode_change_rules: &ChangeControlRulesWASM) {
-        self.0.set_trade_mode_change_rules(trade_mode_change_rules.clone().into());
+    pub fn set_trade_mode_change_rules(
+        &mut self,
+        trade_mode_change_rules: &ChangeControlRulesWASM,
+    ) {
+        self.0
+            .set_trade_mode_change_rules(trade_mode_change_rules.clone().into());
     }
 }
