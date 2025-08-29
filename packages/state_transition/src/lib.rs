c7cdc084d1acd0d48add0b2671b94e75da6e3130
owl352 <kokosinca123@gmail.com>
2025-08-29 19:43:17 +0300

    identity nonce, identity contract nonce and owner getters and setters

--- packages/state_transition/src/lib.rs
+++ packages/state_transition/src/lib.rs
@@ -1,10 +1,30 @@
 use dpp::dashcore::secp256k1::hashes::hex::Case::Lower;
 use dpp::dashcore::secp256k1::hashes::hex::DisplayHex;
+use dpp::data_contract::serialized_version::DataContractInSerializationFormat;
 use dpp::identity::{KeyID, KeyType};
 use dpp::platform_value::BinaryData;
 use dpp::platform_value::string_encoding::{Encoding, decode, encode};
-use dpp::prelude::UserFeeIncrease;
+use dpp::prelude::{IdentityNonce, UserFeeIncrease};
 use dpp::serialization::{PlatformDeserializable, PlatformSerializable, Signable};
+use dpp::state_transition::StateTransition::{
+    Batch, DataContractCreate, DataContractUpdate, IdentityCreditTransfer,
+    IdentityCreditWithdrawal, IdentityTopUp, IdentityUpdate, MasternodeVote,
+};
+use dpp::state_transition::batch_transition::BatchTransition;
+use dpp::state_transition::batch_transition::batched_transition::BatchedTransition;
+use dpp::state_transition::batch_transition::batched_transition::document_transition::DocumentTransitionV0Methods;
+use dpp::state_transition::batch_transition::batched_transition::token_transition::TokenTransitionV0Methods;
+use dpp::state_transition::batch_transition::methods::v0::DocumentsBatchTransitionMethodsV0;
+use dpp::state_transition::data_contract_create_transition::DataContractCreateTransition;
+use dpp::state_transition::data_contract_create_transition::accessors::DataContractCreateTransitionAccessorsV0;
+use dpp::state_transition::data_contract_update_transition::DataContractUpdateTransition;
+use dpp::state_transition::data_contract_update_transition::accessors::DataContractUpdateTransitionAccessorsV0;
+use dpp::state_transition::identity_credit_transfer_transition::accessors::IdentityCreditTransferTransitionAccessorsV0;
+use dpp::state_transition::identity_credit_withdrawal_transition::accessors::IdentityCreditWithdrawalTransitionAccessorsV0;
+use dpp::state_transition::identity_topup_transition::accessors::IdentityTopUpTransitionAccessorsV0;
+use dpp::state_transition::identity_update_transition::accessors::IdentityUpdateTransitionAccessorsV0;
+use dpp::state_transition::masternode_vote_transition::MasternodeVoteTransition;
+use dpp::state_transition::masternode_vote_transition::accessors::MasternodeVoteTransitionAccessorsV0;
 use dpp::state_transition::{
     StateTransition, StateTransitionIdentitySigned, StateTransitionSigningOptions,
 };
@@ -107,7 +127,7 @@ impl StateTransitionWASM {
         let allow_signing_with_any_purpose = js_allow_signing_with_any_purpose.unwrap_or(false);
 
         match &self.0 {
-            StateTransition::DataContractCreate(st) => {
+            DataContractCreate(st) => {
                 st.verify_public_key_level_and_purpose(
                     &public_key.clone().into(),
                     StateTransitionSigningOptions {
@@ -120,7 +140,7 @@ impl StateTransitionWASM {
                 st.verify_public_key_is_enabled(&public_key.clone().into())
                     .with_js_error()?;
             }
-            StateTransition::DataContractUpdate(st) => {
+            DataContractUpdate(st) => {
                 st.verify_public_key_level_and_purpose(
                     &public_key.clone().into(),
                     StateTransitionSigningOptions {
@@ -133,7 +153,7 @@ impl StateTransitionWASM {
                 st.verify_public_key_is_enabled(&public_key.clone().into())
                     .with_js_error()?;
             }
-            StateTransition::Batch(st) => {
+            Batch(st) => {
                 st.verify_public_key_level_and_purpose(
                     &public_key.clone().into(),
                     StateTransitionSigningOptions {
@@ -146,7 +166,7 @@ impl StateTransitionWASM {
                 st.verify_public_key_is_enabled(&public_key.clone().into())
                     .with_js_error()?;
             }
-            StateTransition::IdentityCreditWithdrawal(st) => {
+            IdentityCreditWithdrawal(st) => {
                 st.verify_public_key_level_and_purpose(
                     &public_key.clone().into(),
                     StateTransitionSigningOptions {
@@ -159,7 +179,7 @@ impl StateTransitionWASM {
                 st.verify_public_key_is_enabled(&public_key.clone().into())
                     .with_js_error()?;
             }
-            StateTransition::IdentityUpdate(st) => {
+            IdentityUpdate(st) => {
                 st.verify_public_key_level_and_purpose(
                     &public_key.clone().into(),
                     StateTransitionSigningOptions {
@@ -172,7 +192,7 @@ impl StateTransitionWASM {
                 st.verify_public_key_is_enabled(&public_key.clone().into())
                     .with_js_error()?;
             }
-            StateTransition::IdentityCreditTransfer(st) => {
+            IdentityCreditTransfer(st) => {
                 st.verify_public_key_level_and_purpose(
                     &public_key.clone().into(),
                     StateTransitionSigningOptions {
@@ -185,7 +205,7 @@ impl StateTransitionWASM {
                 st.verify_public_key_is_enabled(&public_key.clone().into())
                     .with_js_error()?;
             }
-            StateTransition::MasternodeVote(st) => {
+            MasternodeVote(st) => {
                 st.verify_public_key_level_and_purpose(
                     &public_key.clone().into(),
                     StateTransitionSigningOptions {
@@ -272,15 +292,15 @@ impl StateTransitionWASM {
     #[wasm_bindgen(js_name = "getActionTypeNumber")]
     pub fn get_action_type_number(&self) -> u8 {
         match self.0 {
-            StateTransition::DataContractCreate(_) => 0,
-            StateTransition::Batch(_) => 1,
+            DataContractCreate(_) => 0,
+            Batch(_) => 1,
             StateTransition::IdentityCreate(_) => 2,
-            StateTransition::IdentityTopUp(_) => 3,
-            StateTransition::DataContractUpdate(_) => 4,
-            StateTransition::IdentityUpdate(_) => 5,
-            StateTransition::IdentityCreditWithdrawal(_) => 6,
-            StateTransition::IdentityCreditTransfer(_) => 7,
-            StateTransition::MasternodeVote(_) => 8,
+            IdentityTopUp(_) => 3,
+            DataContractUpdate(_) => 4,
+            IdentityUpdate(_) => 5,
+            IdentityCreditWithdrawal(_) => 6,
+            IdentityCreditTransfer(_) => 7,
+            MasternodeVote(_) => 8,
         }
     }
 
@@ -339,6 +359,46 @@ impl StateTransitionWASM {
         }
     }
 
+    #[wasm_bindgen(js_name = "getIdentityContractNonce")]
+    pub fn get_identity_contract_nonce(&self) -> Option<IdentityNonce> {
+        match self.0.clone() {
+            DataContractCreate(_) => None,
+            DataContractUpdate(contract_update) => Some(contract_update.identity_contract_nonce()),
+            Batch(batch) => match batch {
+                BatchTransition::V0(v0) => Some(v0.transitions.first()?.identity_contract_nonce()),
+                BatchTransition::V1(v1) => match v1.transitions.first()? {
+                    BatchedTransition::Document(doc_batch) => {
+                        Some(doc_batch.identity_contract_nonce())
+                    }
+                    BatchedTransition::Token(token_batch) => {
+                        Some(token_batch.identity_contract_nonce())
+                    }
+                },
+            },
+            StateTransition::IdentityCreate(_) => None,
+            IdentityTopUp(_) => None,
+            IdentityCreditWithdrawal(_) => None,
+            IdentityUpdate(_) => None,
+            IdentityCreditTransfer(_) => None,
+            MasternodeVote(_) => None,
+        }
+    }
+
+    #[wasm_bindgen(js_name = "getIdentityNonce")]
+    pub fn get_identity_nonce(&self) -> Option<IdentityNonce> {
+        match self.0.clone() {
+            DataContractCreate(contract_create) => Some(contract_create.identity_nonce()),
+            DataContractUpdate(_) => None,
+            Batch(_) => None,
+            StateTransition::IdentityCreate(_) => None,
+            IdentityTopUp(_) => None,
+            IdentityCreditWithdrawal(withdrawal) => Some(withdrawal.nonce()),
+            IdentityUpdate(identity_update) => Some(identity_update.nonce()),
+            IdentityCreditTransfer(credit_transfer) => Some(credit_transfer.nonce()),
+            MasternodeVote(mn_vote) => Some(mn_vote.nonce()),
+        }
+    }
+
     #[wasm_bindgen(setter = "signature")]
     pub fn set_signature(&mut self, signature: Vec<u8>) {
         self.0.set_signature(BinaryData::from(signature))
@@ -352,5 +412,192 @@ impl StateTransitionWASM {
     #[wasm_bindgen(setter = "userFeeIncrease")]
     pub fn set_user_fee_increase(&mut self, user_fee_increase: UserFeeIncrease) {
         self.0.set_user_fee_increase(user_fee_increase)
+    }
+
+    #[wasm_bindgen(js_name = "setOwnerId")]
+    pub fn set_owner_id(&mut self, js_owner_id: &JsValue) -> Result<(), JsValue> {
+        let owner_id = IdentifierWASM::try_from(js_owner_id.clone())?;
+
+        match self.0.clone() {
+            DataContractCreate(mut contract_create) => {
+                let new_contract = match contract_create.data_contract().clone() {
+                    DataContractInSerializationFormat::V0(mut v0) => {
+                        v0.owner_id = owner_id.into();
+
+                        DataContractInSerializationFormat::V0(v0)
+                    }
+                    DataContractInSerializationFormat::V1(mut v1) => {
+                        v1.owner_id = owner_id.into();
+
+                        DataContractInSerializationFormat::V1(v1)
+                    }
+                };
+
+                contract_create.set_data_contract(new_contract);
+
+                self.0 = DataContractCreate(contract_create);
+            }
+            DataContractUpdate(mut contract_update) => {
+                let new_contract = match contract_update.data_contract().clone() {
+                    DataContractInSerializationFormat::V0(mut v0) => {
+                        v0.owner_id = owner_id.into();
+
+                        DataContractInSerializationFormat::V0(v0)
+                    }
+                    DataContractInSerializationFormat::V1(mut v1) => {
+                        v1.owner_id = owner_id.into();
+
+                        DataContractInSerializationFormat::V1(v1)
+                    }
+                };
+
+                contract_update.set_data_contract(new_contract);
+
+                self.0 = DataContractUpdate(contract_update);
+            }
+            Batch(mut batch) => {
+                batch = match batch {
+                    BatchTransition::V0(mut v0) => {
+                        v0.owner_id = owner_id.into();
+
+                        BatchTransition::V0(v0)
+                    }
+                    BatchTransition::V1(mut v1) => {
+                        v1.owner_id = owner_id.into();
+
+                        BatchTransition::V1(v1)
+                    }
+                };
+
+                self.0 = Batch(batch);
+            }
+            StateTransition::IdentityCreate(_) => {
+                Err(JsValue::from_str(
+                    "Cannot set owner for identity create transition",
+                ))?;
+            }
+            IdentityTopUp(mut top_up) => {
+                top_up.set_identity_id(owner_id.into());
+
+                self.0 = IdentityTopUp(top_up);
+            }
+            IdentityCreditWithdrawal(mut withdrawal) => {
+                withdrawal.set_identity_id(owner_id.into());
+
+                self.0 = IdentityCreditWithdrawal(withdrawal);
+            }
+            IdentityUpdate(mut identity_update) => {
+                identity_update.set_identity_id(owner_id.into());
+
+                self.0 = IdentityUpdate(identity_update);
+            }
+            IdentityCreditTransfer(mut credit_transfer) => {
+                credit_transfer.set_identity_id(owner_id.into());
+
+                self.0 = IdentityCreditTransfer(credit_transfer);
+            }
+            MasternodeVote(mut mn_vote) => {
+                mn_vote.set_voter_identity_id(owner_id.into());
+
+                self.0 = MasternodeVote(mn_vote);
+            }
+        };
+
+        Ok(())
+    }
+
+    #[wasm_bindgen(js_name = "setIdentityContractNonce")]
+    pub fn set_identity_contract_nonce(&mut self, nonce: IdentityNonce) -> Result<(), JsValue> {
+        self.0 = match self.0.clone() {
+            DataContractCreate(_) => Err(JsValue::from_str(
+                "Cannot set identity contract nonce for Data Contract Create",
+            ))?,
+            DataContractUpdate(contract_update) => match contract_update {
+                DataContractUpdateTransition::V0(mut v0) => {
+                    v0.identity_contract_nonce = nonce;
+
+                    DataContractUpdateTransition::V0(v0).into()
+                }
+            },
+            Batch(mut batch) => {
+                batch.set_identity_contract_nonce(nonce);
+
+                batch.into()
+            }
+            StateTransition::IdentityCreate(_) => Err(JsValue::from_str(
+                "Cannot set identity contract nonce for Identity Create",
+            ))?,
+            IdentityTopUp(_) => Err(JsValue::from_str(
+                "Cannot set identity contract nonce for Identity Top Up",
+            ))?,
+            IdentityCreditWithdrawal(_) => Err(JsValue::from_str(
+                "Cannot set identity contract nonce for Identity Credit Withdrawal",
+            ))?,
+            IdentityUpdate(_) => Err(JsValue::from_str(
+                "Cannot set identity contract nonce for Identity Update",
+            ))?,
+            IdentityCreditTransfer(_) => Err(JsValue::from_str(
+                "Cannot set identity contract nonce for Identity Credit Transfer",
+            ))?,
+            MasternodeVote(_) => Err(JsValue::from_str(
+                "Cannot set identity contract nonce for Masternode Vote",
+            ))?,
+        };
+
+        Ok(())
+    }
+
+    #[wasm_bindgen(js_name = "setIdentityNonce")]
+    pub fn set_identity_nonce(&mut self, nonce: IdentityNonce) -> Result<(), JsValue> {
+        self.0 = match self.0.clone() {
+            DataContractCreate(mut contract_create) => {
+                contract_create = match contract_create {
+                    DataContractCreateTransition::V0(mut v0) => {
+                        v0.identity_nonce = nonce;
+                        v0.into()
+                    }
+                };
+
+                contract_create.into()
+            }
+            DataContractUpdate(_) => Err(JsValue::from_str(
+                "Cannot set identity nonce for Data Contract Update",
+            ))?,
+            Batch(_) => Err(JsValue::from_str("Cannot set identity nonce for Batch"))?,
+            StateTransition::IdentityCreate(_) => Err(JsValue::from_str(
+                "Cannot set identity nonce for Identity Create",
+            ))?,
+            IdentityTopUp(_) => Err(JsValue::from_str(
+                "Cannot set identity nonce for Identity Top Up",
+            ))?,
+            IdentityCreditWithdrawal(mut withdrawal) => {
+                withdrawal.set_nonce(nonce);
+
+                withdrawal.into()
+            }
+            IdentityUpdate(mut identity_update) => {
+                identity_update.set_nonce(nonce);
+
+                identity_update.into()
+            }
+            IdentityCreditTransfer(mut credit_transfer) => {
+                credit_transfer.set_nonce(nonce);
+
+                credit_transfer.into()
+            }
+            MasternodeVote(mut mn_vote) => {
+                mn_vote = match mn_vote {
+                    MasternodeVoteTransition::V0(mut v0) => {
+                        v0.nonce = nonce;
+
+                        v0.into()
+                    }
+                };
+
+                mn_vote.into()
+            }
+        };
+
+        Ok(())
     }
 }
