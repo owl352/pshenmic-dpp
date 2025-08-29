0836524dc85c078cf19cc064d458b5785ce46e0b
owl352 <kokosinca123@gmail.com>
2025-08-29 20:10:35 +0300

    dataContractId getter and setter in  BatchedTransitionWASM

--- packages/batch/src/batched_transition/mod.rs
+++ packages/batch/src/batched_transition/mod.rs
@@ -1,8 +1,13 @@
 use crate::document_transition::DocumentTransitionWASM;
 use crate::token_transition::TokenTransitionWASM;
 use dpp::state_transition::batch_transition::batched_transition::BatchedTransition;
-use dpp::state_transition::batch_transition::batched_transition::document_transition::DocumentTransition;
-use dpp::state_transition::batch_transition::batched_transition::token_transition::TokenTransition;
+use dpp::state_transition::batch_transition::batched_transition::document_transition::{
+    DocumentTransition, DocumentTransitionV0Methods,
+};
+use dpp::state_transition::batch_transition::batched_transition::token_transition::{
+    TokenTransition, TokenTransitionV0Methods,
+};
+use pshenmic_dpp_identifier::IdentifierWASM;
 use pshenmic_dpp_utils::{IntoWasm, get_class_type};
 use wasm_bindgen::JsValue;
 use wasm_bindgen::prelude::wasm_bindgen;
@@ -69,5 +74,37 @@ impl BatchedTransitionWASM {
                 TokenTransitionWASM::from(token_transition.clone()).into()
             }
         }
+    }
+
+    #[wasm_bindgen(getter = "dataContractId")]
+    pub fn data_contract_id(&self) -> IdentifierWASM {
+        match self.0.clone() {
+            BatchedTransition::Document(document_transition) => {
+                document_transition.data_contract_id().into()
+            }
+            BatchedTransition::Token(token_transition) => {
+                token_transition.data_contract_id().into()
+            }
+        }
+    }
+
+    #[wasm_bindgen(setter = "dataContractId")]
+    pub fn set_data_contract_id(&mut self, js_contract_id: &JsValue) -> Result<(), JsValue> {
+        let contract_id = IdentifierWASM::try_from(js_contract_id)?;
+
+        self.0 = match self.0.clone() {
+            BatchedTransition::Document(mut document_transition) => {
+                document_transition.set_data_contract_id(contract_id.into());
+
+                BatchedTransition::Document(document_transition)
+            }
+            BatchedTransition::Token(mut token_transition) => {
+                token_transition.set_data_contract_id(contract_id.into());
+
+                BatchedTransition::Token(token_transition)
+            }
+        };
+
+        Ok(())
     }
 }
