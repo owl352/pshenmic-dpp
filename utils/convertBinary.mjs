708d58f5aeb83fd998743de7807a79e740ab1a13
owl352 <kokosinca123@gmail.com>
2025-09-26 05:34:45 +0300

    gzip

--- utils/convertBinary.mjs
+++ utils/convertBinary.mjs
@@ -1,14 +1,34 @@
 import fs from 'fs'
 import { encode } from './base122.mjs'
+import zlib from "zlib";
 
 process.argv.forEach(function (val, index) {
   if (val.includes('.wasm')) {
     const binaryData = fs.readFileSync(val)
 
-    const encodedData = Buffer.from(encode(binaryData)).toString('utf-8')
+    const bufferData = Buffer.from(binaryData)
 
-    console.log(`export default "${encodedData}"`)
+    const compressedChunks = [];
 
-    process.exit(0)
+    const gzip = zlib.createGzip({
+      level: 9,
+      memLevel: 9
+    })
+
+    gzip.on('data', data => {
+      compressedChunks.push(...data)
+    })
+
+    gzip.on('end', () => {
+      const encodedData = Buffer.from(encode(compressedChunks)).toString('utf-8')
+
+      console.log(`export default "${encodedData}"`)
+
+      process.exit(0)
+    })
+
+    gzip.write(bufferData)
+
+    gzip.end()
   }
 })
