ba8854e4f5ce9e5c72985d36f6887e61932f2721
owl352 <kokosinca123@gmail.com>
2025-10-21 03:37:54 +0300

    tiny fixes

--- utils/convertBinary.mjs
+++ utils/convertBinary.mjs
@@ -25,7 +25,7 @@ gzip.on('data', data => {
 
 gzip.on('end', () => {
   const encodedData = Buffer.from(encode(compressedChunks)).toString('utf-8');
-  const outputContent = `const wasmBytes = "${encodedData}"\nexport {wasmBytes}`;
+  const outputContent = `const bytes = "${encodedData}"\nexport {bytes}`;
 
   fs.writeFileSync(outputFile, outputContent);
 
