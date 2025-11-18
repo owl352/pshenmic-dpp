3b50341fb4af6ce6121704f87e434b587fbd3328
owl352 <kokosinca123@gmail.com>
2025-11-18 09:28:58 +0300

    typedoc

--- pkg/src/wasm.ts
+++ pkg/src/wasm.ts
@@ -1,7 +1,11 @@
 import * as protocol from '../binaries/wasm.js'
-import { setDpp } from './dpp/dpp.js'
+import {setDpp} from './dpp/dpp.js'
 
 setDpp(protocol)
 
-export { KeyType, Purpose, PlatformVersionWASM, NetworkWASM, SecurityLevel } from './enums.js'
-export { IdentityWASM, IdentifierWASM, IdentityPublicKeyWASM } from './dpp/dpp.js'
+export {KeyType, Purpose, PlatformVersionWASM, NetworkWASM, SecurityLevel} from './enums.js'
+export {IdentityWASM, IdentifierWASM, IdentityPublicKeyWASM} from './dpp/dpp.js'
+
+export {
+  KeyTypeLike, NetworkLike, SecurityLevelLike, PlatformVersionLike, PurposeLike, EnumLike, DashPlatformProtocol
+} from './types.js'
