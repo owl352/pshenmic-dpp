dd87ce83eb5e51db5e28f0f2239c2c57998d1340
owl352 <kokosinca123@gmail.com>
2025-12-15 16:43:45 +0300

    lint

--- tests/IdentityCreateTransition.spec.cjs
+++ tests/IdentityCreateTransition.spec.cjs
@@ -1,7 +1,6 @@
 const assert = require('assert')
 const { describe, it } = require('mocha')
 const { default: wasm } = require('..')
-const { fromHexString } = require('./utils/hex')
 
 describe('IdentityCreateTransition', function () {
   describe('serialization / deserialization', function () {
