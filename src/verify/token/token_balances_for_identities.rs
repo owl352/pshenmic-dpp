274441aedfc4d0718799a32d07d76ffc9d9d68ac
owl352 <kokosinca123@gmail.com>
2026-02-25 22:12:57 +0300

    add IdentityTokenBalanceOptionalNAPI

--- src/verify/token/token_balances_for_identities.rs
+++ src/verify/token/token_balances_for_identities.rs
@@ -7,19 +7,33 @@ use crate::{
     dynamic_value::{BigIntString, DynamicValue, IdentifierLikeNAPI, TryToU64},
     enums::platform_version::PlatformVersionNAPI,
     identifier::IdentifierNAPI,
-    verify::state_transition::entities::VerifiedIdentityBalanceNAPI,
 };
+
+#[derive(Clone)]
+#[napi(js_name = "IdentityTokenBalanceOptionalNAPI")]
+pub struct IdentityTokenBalanceOptionalNAPI {
+    pub(crate) id: IdentifierNAPI,
+    pub balance: Option<BigIntString>,
+}
+
+#[napi]
+impl IdentityTokenBalanceOptionalNAPI {
+    #[napi(getter, js_name = "id")]
+    pub fn get_id(&self) -> IdentifierNAPI {
+        self.id.clone()
+    }
+}
 
 #[napi(js_name = "VerifiedTokenBalancesForIdentitiesNAPI")]
 pub struct VerifiedTokenBalancesForIdentitiesNAPI {
     pub root_hash: Uint8Array,
-    balances: Vec<VerifiedIdentityBalanceNAPI>,
+    balances: Vec<IdentityTokenBalanceOptionalNAPI>,
 }
 
 #[napi]
 impl VerifiedTokenBalancesForIdentitiesNAPI {
     #[napi(getter, js_name = "balances")]
-    pub fn balances(&self) -> Vec<VerifiedIdentityBalanceNAPI> {
+    pub fn balances(&self) -> Vec<IdentityTokenBalanceOptionalNAPI> {
         self.balances.clone()
     }
 }
@@ -50,15 +64,12 @@ pub fn verify_token_balances_for_identities(
         )
         .map_err(|e| napi::Error::new(napi::Status::GenericFailure, e.to_string()))?;
 
-    let mut js_balances: Vec<VerifiedIdentityBalanceNAPI> = Vec::new();
+    let mut js_balances: Vec<IdentityTokenBalanceOptionalNAPI> = Vec::new();
 
     for (id, balance) in balances_vec.iter() {
-        js_balances.push(VerifiedIdentityBalanceNAPI {
+        js_balances.push(IdentityTokenBalanceOptionalNAPI {
             id: IdentifierNAPI::from(id.clone()),
-            balance: balance
-                .clone()
-                .map(BigIntString::from_u64)
-                .unwrap_or("-1".to_string()),
+            balance: balance.clone().map(BigIntString::from_u64),
         });
     }
 
