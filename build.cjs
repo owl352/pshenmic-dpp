3d8cf601206caf3025a0af4dcab16daff0a40bc1
owl352 <kokosinca123@gmail.com>
2026-03-16 23:02:51 +0300

    update build

--- build.cjs
+++ build.cjs
@@ -67,11 +67,12 @@ async function main() {
     try {
       console.log(`Building for target: ${target}...`);
 
+      const existingRUSTFLAGS = process.env.RUSTFLAGS || "";
       await execTask(
         `cargo zigbuild --target ${target} ${isRelease ? "--release" : ""}`,
         { env: {
             ...process.env,
-            RUSTFLAGS: "--crate-type=cdylib"
+            RUSTFLAGS: `${existingRUSTFLAGS} --crate-type=cdylib`.trim()
           }}
       );
 
