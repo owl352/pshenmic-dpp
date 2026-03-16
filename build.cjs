26e63d2a58e69464d207ea169849547ca17a4b61
owl352 <kokosinca123@gmail.com>
2026-03-17 00:09:33 +0300

    enable linux x86

--- build.cjs
+++ build.cjs
@@ -67,12 +67,10 @@ async function main() {
     try {
       console.log(`Building for target: ${target}...`);
 
-      // const existingRUSTFLAGS = process.env.RUSTFLAGS || "";
       await execTask(
         `cargo zigbuild --target ${target} ${isRelease ? "--release" : ""}`,
         { env: {
             ...process.env,
-            // RUSTFLAGS: `${existingRUSTFLAGS} --crate-type=cdylib`.trim()
           }}
       );
 
